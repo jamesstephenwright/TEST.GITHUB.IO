8ff0827a2a756f3b298425e49fd7525268442456
jamesstephenwright <160615728+jamesstephenwright@users.noreply.github.com>
2024-05-15 10:36:30 +0100

    update

--- proxy.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Proxy script to fetch and serve content
-$url = "https://storymaps.arcgis.com/stories/57db70af523047f9ba4f730f41681d98";
-$content = file_get_contents($url);
-echo $content;
-?>
